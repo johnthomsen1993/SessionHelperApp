a5cf0a2cd2b2aec874c04fe662dd88a355a95767
John Thomsen <John.Thomsen@emea.merkleinc.com>
2023-04-21 10:44:18 +0200

    Initial Commit

---
+++ SessionHelperApp/SessionHelperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionHelperAppApp.swift
+//  SessionHelperApp
+//
+//  Created by John Thomsen on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SessionHelperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
